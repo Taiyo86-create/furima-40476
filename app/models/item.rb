170634baa79e0f3f56d6e2ede96ac7f2ab3e2d65
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-03-22 21:11:14 +0900

    アクティブハッシュでの実装完了
    発送元,目安のアクティブハッシュの追加
    
    各バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,22 @@ class Item < ApplicationRecord
 
   validates :category_id, numericality: { other_than: 1 }
 
-  # カテゴリーのアクティブハッシュ
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :schedule
 
+
+  validates :itemsName, :text, presence: true
+  validates :itemsFeature, :text, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :delivery_charge_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :schedule_id, numericality: { other_than: 1 }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_114950) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_22_120145) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -62,6 +62,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_22_114950) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "prefectures", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schedules", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "statuses", charset: "utf8", force: :cascade do |t|
