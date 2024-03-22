e6b0888d7a2cc1b98633bd19f25ead9eba8718ad
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-03-22 18:35:31 +0900

    データベース保存機能
    itemsコントローラーのcreateアクションの定義
    写真投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,14 @@ class ItemsController < ApplicationController
   def new
 
   end
+
+  def create
+    Item.create(item_params)
+    redirect_to items_path
+  end
+
+  private
+  def item_params
+  params.require(:item).permit(:itemsName, :itemsFeature, :category_id, :price, :status_id, :delivery_charge_id, :prefecture_id, :schedule_id, :use, :image )
+  end
 end
