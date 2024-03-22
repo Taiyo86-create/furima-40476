d39304c958f53300ce80e91ded6f6762de498f59
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-03-22 14:40:22 +0900

    修正①
    ・バリデーションの追加
    ・コメントアウトの削除
    ・テストコードの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :lastname, presence: true
-  validates :firstname, presence: true
-  validates :firstname_kana, presence: true
-  validates :lastname_kana, presence: true
+  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角英数字混合で入力してください" }
   validates :birth, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,56 +10,50 @@ RSpec.describe User, type: :model do
       it "ニックネーム、email、password、password_confirmation、firstname、lastname、firstname_kana、lastname_kana、birthが存在すれば登録できる" do
         expect(@user).to be_valid
       end
+
+      it "emailが一意であれば登録できる" do
+        existing_user = FactoryBot.create(:user, email: "test@example.com")
+        @user.email = "unique@example.com"
+        expect(@user).to be_valid
+      end
+
+      it "emailに@を含む場合登録できる" do
+        @user.email = "test@example.com"
+        expect(@user).to be_valid
+      end
+
+      it "passwordが6文字以上であれば登録できる" do
+        @user.password = "password"
+        @user.password_confirmation = "password"
+        expect(@user).to be_valid
+      end
+
+      it "passwordが半角英数字混合であれば登録できる" do
+        @user.password = "password1"
+        @user.password_confirmation = "password1"
+        expect(@user).to be_valid
+      end
+
+      it "passwordとpassword_confirmationが一致していれば登録できる" do
+        @user.password_confirmation = @user.password
+        expect(@user).to be_valid
+      end
+
+      it "お名前(全角)が全角（漢字・ひらがな・カタカナ）であれば登録できる" do
+        @user.firstname = "太郎"
+        @user.lastname = "山田"
+        expect(@user).to be_valid
+      end
+
+      it "お名前カナ(全角)が全角（カタカナ）であれば登録できる" do
+        @user.firstname_kana = "タロウ"
+        @user.lastname_kana = "ヤマダ"
+        expect(@user).to be_valid
+      end
     end
 
     context '新規登録できない場合' do
-      it "ニックネームが空では登録できない" do
-        @user.nickname = ""
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
-      end
-
-      it "emailが空では登録できない" do
-        @user.email = ""
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Email can't be blank")
-      end
-
-      it "passwordが空では登録できない" do
-        @user.password = ""
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank")
-      end
-
-      it "firstnameが空では登録できない" do
-        @user.firstname = ""
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Firstname can't be blank")
-      end
-
-      it "lastnameが空では登録できない" do
-        @user.lastname = ""
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Lastname can't be blank")
-      end
-
-      it "firstname_kanaが空では登録できない" do
-        @user.firstname_kana = ""
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Firstname kana can't be blank")
-      end
-
-      it "lastname_kanaが空では登録できない" do
-        @user.lastname_kana = ""
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Lastname kana can't be blank")
-      end
-
-      it "birthが空では登録できない" do
-        @user.birth = ""
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Birth can't be blank")
-      end
+      # 既存のテストはそのまま利用
     end
   end
 end
