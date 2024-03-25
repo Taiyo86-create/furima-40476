5c0494a598ccc9cd8f4d3821fe57d9a784c3a0c0
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-03-25 16:50:00 +0900

    テストコード修正
    正常系異常系に分けました

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
     itemsName { Faker::Commerce.product_name }
     itemsFeature { Faker::Lorem.sentence }
     price { Faker::Commerce.price(range: 1000..100000).to_i }
-    status { Status.all.sample }
-    delivery_charge { DeliveryCharge.all.sample }
-    prefecture { Prefecture.all.sample }
-    schedule { Schedule.all.sample }
+    status_id { Status.all.sample }
+    delivery_charge_id { DeliveryCharge.all.sample }
+    prefecture_id { Prefecture.all.sample }
+    schedule_id { Schedule.all.sample }
     user { association :user }
-    category { Category.all.sample}
+    category_id { Category.all.sample}
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,22 +3,84 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    # @user = create(:user)
-    # @category = create(:category)
-    # @status = create(:status)
-    # @delivery_charge = create(:delivery_charge)
-    # @prefecture = create(:prefecture)
-    # @schedule = create(:schedule)
-    # @price = 1000
-    @item = FactoryBot.build(:item )
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item, user: @user, category_id: FactoryBot.create(:category).id, status_id: FactoryBot.create(:status).id, delivery_charge_id: FactoryBot.create(:delivery_charge).id, prefecture_id: FactoryBot.create(:prefecture).id, schedule_id: FactoryBot.create(:schedule).id)
   end
 
 
-  it "is valid with valid attributes" do
-    expect(@item).to be_valid
+  # 正常系テスト
+  context "商品保存できる" do
+    it "ログインしたユーザーは出品できる" do
+      expect(@item).to be_valid
+    end
   end
 
-  it "is invalid without a name" do
-    expect(@item).not_to be_valid
+  # 異常系テスト
+  context "when user is not logged in" do
+    it "商品名が空だと出品できない" do
+      item = FactoryBot.build(:item, itemsName: "")
+      item.valid?
+      expect(item.errors[:itemsName]).to include("can't be blank")
+    end
+
+    it "商品の特徴が空だと出品できない" do
+      item = FactoryBot.build(:item, itemsFeature: "")
+      item.valid?
+      expect(item.errors[:itemsFeature]).to include("can't be blank")
+    end
+
+    it "価格が空だと出品できない" do
+      item = FactoryBot.build(:item, price: nil)
+      item.valid?
+      expect(item.errors[:price]).to include("can't be blank")
+    end
+
+    it "価格が半角数字以外だと出品できない" do
+      item = FactoryBot.build(:item, price: "１０００")
+      item.valid?
+      expect(item.errors[:price]).to include("is not a number")
+    end
+
+    it "価格の値が300未満の場合は出品できない" do
+      item = FactoryBot.build(:item, price: 299)
+      item.valid?
+      expect(item.errors[:price]).to include("must be greater than or equal to 300")
+    end
+
+    it "価格の値が9999999より大きい場合は出品できない" do
+      item = FactoryBot.build(:item, price: 10000000)
+      item.valid?
+      expect(item.errors[:price]).to include("must be less than or equal to 9999999")
+    end
+
+    it "category_idが1以下の場合は出品できない" do
+      item = FactoryBot.build(:item, category_id: 1)
+      item.valid?
+      expect(item.errors[:category_id]).to include("must be other than 1")
+    end
+
+    it "prefecture_idが1以下の場合は出品できない" do
+      item = FactoryBot.build(:item, prefecture_id: 1)
+      item.valid?
+      expect(item.errors[:prefecture_id]).to include("must be other than 1")
+    end
+
+    it "delivery_charge_idが1以下の場合は出品できない" do
+      item = FactoryBot.build(:item, delivery_charge_id: 1)
+      item.valid?
+      expect(item.errors[:delivery_charge_id]).to include("must be other than 1")
+    end
+
+    it "status_idが1以下の場合は出品できない" do
+      item = FactoryBot.build(:item, status_id: 1)
+      item.valid?
+      expect(item.errors[:status_id]).to include("must be other than 1")
+    end
+
+    it "schedule_idが1以下の場合は出品できない" do
+      item = FactoryBot.build(:item, schedule_id: 1)
+      item.valid?
+      expect(item.errors[:schedule_id]).to include("must be other than 1")
+    end
   end
 end
