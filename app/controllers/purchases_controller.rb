c5ca6922be2b113f00f28bfc6de75dbee886c5df
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-03-26 20:16:04 +0900

    適当なユーザー以外購入できない

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,5 +2,9 @@ class PurchasesController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    unless user_signed_in? && current_user != @item.user
+      redirect_to user_signed_in? ? root_path : new_user_session_path
+    end
   end
+
 end
